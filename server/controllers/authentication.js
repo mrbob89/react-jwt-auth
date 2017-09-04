61359e720ce69959924ea321c66979970cf574a2
Roman Boboshko <romanboboshko89@gmail.com>
2017-09-05 01:36:27 +0300

    Update auth controller

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,28 @@
+const User = require('../models/user');
+
 exports.signup = (req, res, next) => {
-  res.send({ success: true });
-}
+  const { email, password } = req.body;
+
+  User.findOne({ email }, (err, existingUser) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+    const user = new User({
+      email,
+      password
+    });
+
+    user.save(err => {
+      if (err) {
+        return next(err);
+      }
+
+      res.json({ success: true });
+    });
+  });
+};
