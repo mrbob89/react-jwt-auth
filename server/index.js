617eb934cc569819f33027c03a25e3d16357b949
Roman Boboshko <romanboboshko89@gmail.com>
2017-09-05 01:17:29 +0300

    Add basic user model and controller

--- server/index.js
+++ server/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const router = require('./router');
+const keys = require('./config/dev');
 
 const app = express();
+
+// DB Setup
+mongoose.connect(keys.mongoURI);
 
 // App Setup
 app.use(morgan('combined'));
