7e103198b4165c3625f080a25380186795daa89e
Roman Boboshko <romanboboshko89@gmail.com>
2017-09-05 01:48:19 +0300

    Add presave hook to encrypt password

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -3,6 +3,10 @@ const User = require('../models/user');
 exports.signup = (req, res, next) => {
   const { email, password } = req.body;
 
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide email and password' });
+  }
+
   User.findOne({ email }, (err, existingUser) => {
     if (err) {
       return next(err);
--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,27 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 const { Schema } = mongoose;
 
 // Define model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+// On Save Hook, encrypt password
+userSchema.pre('save', next => {
+  const user = this;
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err); }
+
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) { return next(err); }
+
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 // Create the model class
