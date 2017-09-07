83f5fb8b566900f784efe512168759a55fd8fd97
Roman Boboshko <romanb@compandsave.com>
2017-09-07 13:29:36 +0300

    Added token to the request, close routes and finish logic

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER, AUTH_ERROR, UNAUTH_USER } from './types';
+import { AUTH_USER, AUTH_ERROR, UNAUTH_USER, FETCH_MESSAGE } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -47,4 +47,15 @@ export const authError = error => {
         type: AUTH_ERROR,
         payload: error
     };
+};
+
+export const fetchMessage = () => async dispatch => {
+    const response = await axios.get(ROOT_URL, {
+        headers: { authorization: localStorage.getItem('token') }
+    });
+
+    dispatch({
+        type: FETCH_MESSAGE,
+        payload: response.data.message
+    });
 };
--- client/src/index.js
+++ client/src/index.js
@@ -4,23 +4,37 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
+import { AUTH_USER } from './actions/types';
 
 import App from './App';
 import Signin from './components/auth/Signin';
 import Signout from './components/auth/Signout';
 import Signup from './components/auth/Signup';
+import Feature from './components/Feature';
+import Welcome from './components/Welcome';
 import rootReducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
+import RequireAuth from './components/auth/RequireAuth';
+
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(rootReducer);
+
+const token = localStorage.getItem('token');
+
+if (token) {
+    store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(rootReducer)}>
+    <Provider store={store}>
         <Router history={browserHistory}>
             <Route path="/" component={App}>
+                <IndexRoute component={Welcome} />
                 <Route path="signin" component={Signin} />
                 <Route path="signout" component={Signout} />
                 <Route path="signup" component={Signup} />
+                <Route path="feature" component={RequireAuth(Feature)} />
             </Route>
         </Router>
     </Provider>,
